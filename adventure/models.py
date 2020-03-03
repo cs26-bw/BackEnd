4c8c7d839fcb4e33aeaff297a408059924572dc6
Luis Mendes <cvlopes88@gmail.com>
2020-03-03 16:57:02 -0500

    some progress in connecting the words

--- adventure/models.py
+++ adventure/models.py
@@ -12,6 +12,9 @@ class Room(models.Model):
     s_to = models.IntegerField(default=0)
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
@@ -19,18 +22,15 @@ class Room(models.Model):
         except Room.DoesNotExist:
             print("That room does not exist")
         else:
-            if direction == "n":
-                self.n_to = destinationRoomID
-            elif direction == "s":
-                self.s_to = destinationRoomID
-            elif direction == "e":
-                self.e_to = destinationRoomID
-            elif direction == "w":
-                self.w_to = destinationRoomID
-            else:
-                print("Invalid direction")
+            reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
+            reverse_dir = reverse_dirs[direction]
+            if not reverse_dir:
+                print("invalid direction")
                 return
+            setattr(self, f"{direction}_to", destinationRoom)
+            setattr(destinationRoom, f"{reverse_dir}_to", self)
             self.save()
+            destinationRoom.save()
     def playerNames(self, currentPlayerID):
         return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
     def playerUUIDs(self, currentPlayerID):
--- util/create_world.py
+++ util/create_world.py
@@ -1,47 +1,169 @@
 from django.contrib.auth.models import User
 from adventure.models import Player, Room
+import random
+
+class World:
+    def __init__(self):
+        self.grid = None
+        self.width = 0
+        self.height = 0
+        self.starting_room = None
+
+    def generate_rooms(self, size_x, size_y, num_rooms):
+        '''
+        Fill up the grid, bottom to top, in a zig-zag pattern
+        '''
+
+        # Initialize the grid
+        self.grid = [None] * size_y
+        self.width = size_x
+        self.height = size_y
+        for i in range( len(self.grid) ):
+            self.grid[i] = [None] * size_x
+
+        # Start from lower-left corner (0,0)
+        x = random.randint(0, size_x - 2) # (this will become 0 on the first step)
+        y = random.randint(0, size_y - 1)
+        room_count = 0
+
+
+        directions = []
+        if y < size_y -1:
+            directions.append(0)
+        if y > 0:
+            directions.append(2)
+        if x < size_x -1:
+            directions.append(1)
+        if x > 0:
+            directions.append(3)
+
+        # Start generating rooms to the east
+        direction = random.choice(directions)  # 1: east, -1: west
+
+
+        # While there are rooms to be created...
+        previous_room = None
+        while room_count < num_rooms:
+            if direction == 0:
+                room_direction = "n"
+                y += 1
+            elif direction == 1:
+                room_direction = "e"
+                x += 1
+            elif direction == 2:
+                room_direction = "s"
+                y -= 1
+            else:
+                room_direction = "w"
+                x -= 1
+            existing = self.grid[y][x]
+            if not existing:
+                room = Room(title="A Generic Room", description="This is a generic room.", x = x, y = y)
+                # Note that in Django, you'll need to save the room after you create i
+                # Save the room in the World grid
+                self.grid[y][x] = room
+                if not self.starting_room:
+                    self.starting_room = room
+                room_count += 1
+                room.save()
+            else:
+                room = existing
+
+            # Connect the new room to the previous room
+            if previous_room is not None:
+                previous_room.connectRooms(room, room_direction)
+
+            # Update iteration variables
+            previous_room = room
+
+
+            directions = []
+            if y < size_y - 1:
+                directions.append(0)
+            if y > 0:
+                directions.append(2)
+            if x < size_x - 1:
+                directions.append(1)
+            if x > 0:
+                directions.append(3)
+
+            # Start generating rooms to the east
+            direction = random.choice(directions)  # 1: east, -1: west
+
+
+
+    def print_rooms(self):
+        '''
+        Print the rooms in room_grid in ascii characters.
+        '''
+
+        # Add top border
+        str = "# " * ((3 + self.width * 5) // 2) + "\n"
+
+        # The console prints top to bottom but our array is arranged
+        # bottom to top.
+        #
+        # We reverse it so it draws in the right direction.
+        reverse_grid = list(self.grid) # make a copy of the list
+        reverse_grid.reverse()
+        for row in reverse_grid:
+            # PRINT NORTH CONNECTION ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.n_to is not None:
+                    str += "  |  "
+                else:
+                    str += "     "
+            str += "#\n"
+            # PRINT ROOM ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.w_to is not None:
+                    str += "-"
+                else:
+                    str += " "
+                if room is not None:
+                    str += f"{room.id}".zfill(3)
+                else:
+                    str += "   "
+                if room is not None and room.e_to is not None:
+                    str += "-"
+                else:
+                    str += " "
+            str += "#\n"
+            # PRINT SOUTH CONNECTION ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.s_to is not None:
+                    str += "  |  "
+                else:
+                    str += "     "
+            str += "#\n"
+
+        # Add bottom border
+        str += "# " * ((3 + self.width * 5) // 2) + "\n"
+
+        # Print string
+        print(str)
+
+
+w = World()
+num_rooms = 100
+width = 15
+height = 10
+w.generate_rooms(width, height, num_rooms)
+w.print_rooms()
+
+
+print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
 
 
 Room.objects.all().delete()
 
-r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
 
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east.""")
-
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""")
-
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air.""")
-
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""")
-
-r_outside.save()
-r_foyer.save()
-r_overlook.save()
-r_narrow.save()
-r_treasure.save()
-
-# Link rooms together
-r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
-
-r_foyer.connectRooms(r_overlook, "n")
-r_overlook.connectRooms(r_foyer, "s")
-
-r_foyer.connectRooms(r_narrow, "e")
-r_narrow.connectRooms(r_foyer, "w")
-
-r_narrow.connectRooms(r_treasure, "n")
-r_treasure.connectRooms(r_narrow, "s")
 
 players=Player.objects.all()
 for p in players:
-  p.currentRoom=r_outside.id
-  p.save()
+ p.currentRoom= w.starting_room.id
+ p.save()
 
